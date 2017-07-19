3bbef1981273c833893b3b39ffd6d302f567d8b0
xynxynxyn <pinke.till@outlook.com>
2017-07-19 22:16:12 +0200

    clean up yasp info

--- commands/dotabuff/dbinfo.js
+++ commands/dotabuff/dbinfo.js
@@ -6,50 +6,62 @@ const commando = require('discord.js-commando')
 class getDBinfo extends commando.Command {
     constructor(client) {
         super(client, {
-            name: 'db',
+            name: 'yasp',
             group: 'dotabuff',
-            memberName: 'db',
+            memberName: 'yasp',
             description: 'Gives information about a profile',
             args: [
                 {
                     key: 'id',
                     prompt: 'provide a player ID',
                     type: 'integer'
+                },
+                {
+                    key: 'option',
+                    prompt: 'options are mmr and wr',
+                    type: 'string',
+                    default: 'mmr'
                 }
             ]
         })
     }
 
     async run(message, args) {
-        var reply = ''
-        var pointer = [reply]
-        const { id } = args
+        const { id, option } = args
         var getJSON = require('get-json')
+        if(option == 'mmr'){
+            getJSON('https://api.opendota.com/api/players/' + id, function(err, data){
+                var mmr = data['solo_competitive_rank']
+                var partymmr = data['competitive_rank']
+                var estimate = data['mmr_estimate']['estimate'] 
 
-        message.channel.send(
-            ">Statistcs for " + message.author.username,
-        )
-        getJSON('https://api.opendota.com/api/players/' + id, function(err, data){
-            var mmr = data['solo_competitive_rank']
-            var estimate = data['mmr_estimate']['estimate'] 
-            pointer[0] = ''
-            pointer[0] += 'Solo MMR: ' + mmr + '\n'
-            pointer[0] += 'MMR Estimate: ' + estimate + '\n'
-            pointer[0]=data
+                message.channel.send({ embed: {
+                    color: 3447003,
+                    author: { 
+                        name: message.author.username,
+                        icon_url: message.author.avatarURL
+                    },
+                    description: 'Solo MMR: ' + mmr + '\nParty MMR: ' + partymmr + '\nMMR Estimate: ' + estimate
+                }})
+            })
+        }else if(option == 'wr'){
+            getJSON('https://api.opendota.com/api/players/' + id + '/wl', function(err, data){
+                var wl = data['win'] + '/' + data['lose']
+                message.channel.send({ embed: {
+                    color: 3447003,
+                    author: {
+                        name: message.author.username,
+                        icon_url: message.author.avatarURL
+                    },
+                    description: 'W/L: ' + wl
+                }})
 
+            })
+        }else{
             message.channel.send(
-                'Solo MMR: ' + mmr + '\nMMR Estimate: ' + estimate
+                'unknown option'
             )
-        })
-
-        getJSON('https://api.opendota.com/api/players/' + id + '/wl', function(err, data){
-            var wl = data['win'] + '/' + data['lose']
-            pointer[0] += 'W/L:  ' + wl
-            message.channel.send(
-                'W/L: ' + wl
-            )
-
-        })
+        }
     }
 }
 
