edd43ba154dd77a0f89abdc3a8d6524453923a50
xynxynxyn <pinke.till@outlook.com>
2017-07-20 16:50:02 +0200

    clean up yasp command and add local storage for user ids

--- commands/dotabuff/dbinfo.js
+++ commands/dotabuff/dbinfo.js
@@ -12,24 +12,56 @@ class getDBinfo extends commando.Command {
             description: 'Gives information about a profile',
             args: [
                 {
-                    key: 'id',
-                    prompt: 'provide a player ID',
-                    type: 'integer'
-                },
-                {
                     key: 'option',
                     prompt: 'options are mmr and wr',
                     type: 'string',
                     default: 'mmr'
+                },
+                {
+                    key: 'id',
+                    prompt: 'provide a player ID',
+                    type: 'string',
+                    default: '0'
                 }
             ]
         })
     }
 
     async run(message, args) {
-        const { id, option } = args
+        var { option, id } = args
+        console.log('From ' + message.author.username + ', input: ' + option + ' ' + id)
         var getJSON = require('get-json')
-        if(option == 'mmr'){
+        var fs = require('fs')
+        var players = fs.readFileSync('players.txt','utf8')
+        var players_data = JSON.parse(players)
+        for(var i=0; i<players_data.length; i+=1){
+            if(players_data[i][0] == message.author.id){
+                var player_ID = players_data[i][1]
+            }
+        }
+
+        if(option == 'reg'){
+            for(var i=0; i<players_data.length; i+=1){
+                if(players_data[i][0] == message.author.id){
+                    players_data[i][1] = id
+                    console.log(message.author.username + ' already registered a ID \nOverwriting...')
+                    var Overwritten = true;
+                }
+            }
+            if(!Overwritten){
+                players_data.push([message.author.id, id])
+                console.log(message.author.username + ' does not have an ID \nAdding entry...')
+            }
+
+            fs.writeFileSync('players.txt', JSON.stringify(players_data), function(err){
+                if(err){
+                    console.log('something done fucked up')
+                }
+            })
+        }else if(option == 'mmr'){
+            if(id == '0'){
+                id = player_ID
+            }
             getJSON('https://api.opendota.com/api/players/' + id, function(err, data){
                 var mmr = data['solo_competitive_rank']
                 var partymmr = data['competitive_rank']
@@ -38,21 +70,20 @@ class getDBinfo extends commando.Command {
                 message.channel.send({ embed: {
                     color: 3447003,
                     author: { 
-                        name: message.author.username,
-                        icon_url: message.author.avatarURL
+                        name: data['profile']['personaname'],
+                        icon_url: data['profile']['avatarmedium']
                     },
                     description: 'Solo MMR: ' + mmr + '\nParty MMR: ' + partymmr + '\nMMR Estimate: ' + estimate
                 }})
             })
         }else if(option == 'wr'){
+            if(id == '0'){
+                id = player_ID
+            }
             getJSON('https://api.opendota.com/api/players/' + id + '/wl', function(err, data){
                 var wl = data['win'] + '/' + data['lose']
                 message.channel.send({ embed: {
                     color: 3447003,
-                    author: {
-                        name: message.author.username,
-                        icon_url: message.author.avatarURL
-                    },
                     description: 'W/L: ' + wl
                 }})
 
