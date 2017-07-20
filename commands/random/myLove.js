2a4e4435090ef9629d65f7b705b07deeceab9620
xynxynxyn <pinke.till@outlook.com>
2017-07-20 21:43:56 +0200

    removed unnecessary variable

---
+++ commands/random/myLove.js
@@ -0,0 +1,38 @@
+const commando = require('discord.js-commando')
+
+class myLove extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'mylove',
+            group: 'random',
+            memberName: 'mylove',
+            description: 'shows your true love'
+        })
+    }
+
+    async run(message, args) {
+        var member_amount = message.guild.memberCount
+        var members = message.guild.members
+        var members_list = members.array()
+        var random_member = members.random()
+        var i = 0
+        while(true){
+            if(members_list[i].presence.status == 'offline'){
+                if(i>-1){
+                    members_list.splice(i, 1)
+                }
+            }else{
+                i += 1
+            }
+
+            if(i == members_list.length){
+                break
+            }
+        }
+        var x = Math.floor(Math.random()*members_list.length) + 1
+        message.channel.send('Hey ' + message.author.username + ' your true love is ' + members_list[x].user.username)
+
+    }
+}
+
+module.exports = myLove
